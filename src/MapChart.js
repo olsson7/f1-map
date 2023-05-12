d8d0d1e4441bc5d48266d1e5ad3d123face45f30
olsson7 <72479962+olsson7@users.noreply.github.com>
2023-05-12 02:09:36 +0200

    added marker for miami

--- src/MapChart.js
+++ src/MapChart.js
@@ -1,77 +1,82 @@
 import React, { useState } from "react";
 import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  ZoomableGroup
+    ComposableMap,
+    Geographies,
+    Geography,
+    ZoomableGroup,
+    Marker
 } from "react-simple-maps";
 
 const geoUrl = "/features.json";
 
 const MapChart = () => {
-  const [position, setPosition] = useState({ coordinates: [0, 0], zoom: 1 });
+    const [position, setPosition] = useState({ coordinates: [0, 0], zoom: 1 });
 
-  function handleZoomIn() {
-    if (position.zoom >= 4) return;
-    setPosition((pos) => ({ ...pos, zoom: pos.zoom * 1.33 }));
-  }
+    function handleZoomIn() {
+        if (position.zoom >= 4) return;
+        setPosition((pos) => ({ ...pos, zoom: pos.zoom * 1.33 }));
+    }
 
-  function handleZoomOut() {
-    if (position.zoom <= 1) return;
-    setPosition((pos) => ({ ...pos, zoom: pos.zoom / 1.33 }));
-  }
+    function handleZoomOut() {
+        if (position.zoom <= 1) return;
+        setPosition((pos) => ({ ...pos, zoom: pos.zoom / 1.33 }));
+    }
 
-  function handleMoveEnd(position) {
-    setPosition(position);
-  }
+    function handleMoveEnd(position) {
+        setPosition(position);
+    }
 
-  return (
-    <div>
-        <h1> F1 World map!</h1>
-      <ComposableMap>
-        <ZoomableGroup
-          zoom={position.zoom}
-          center={position.coordinates}
-          onMoveEnd={handleMoveEnd}
-        >
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography key={geo.rsmKey} geography={geo} />
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
-      <div className="controls">
-        <button onClick={handleZoomIn}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="3"
-          >
-            <line x1="12" y1="5" x2="12" y2="19" />
-            <line x1="5" y1="12" x2="19" y2="12" />
-          </svg>
-        </button>
-        <button onClick={handleZoomOut}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="3"
-          >
-            <line x1="5" y1="12" x2="19" y2="12" />
-          </svg>
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h1> F1 World map!</h1>
+            <ComposableMap>
+                <ZoomableGroup
+                    zoom={position.zoom}
+                    center={position.coordinates}
+                    onMoveEnd={handleMoveEnd}
+                >
+                    <Geographies geography={geoUrl}>
+                        {({ geographies }) =>
+                            geographies.map((geo) => (
+                                <Geography key={geo.rsmKey} geography={geo} />
+                            ))
+                        }
+                    </Geographies>
+                    <Marker coordinates={[-80.1918, 25.7617]}>
+                        <circle r={3} fill="#F53" />
+                    </Marker>
+                </ZoomableGroup>
+
+            </ComposableMap>
+            <div className="controls">
+                <button onClick={handleZoomIn}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="3"
+                    >
+                        <line x1="12" y1="5" x2="12" y2="19" />
+                        <line x1="5" y1="12" x2="19" y2="12" />
+                    </svg>
+                </button>
+                <button onClick={handleZoomOut}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="3"
+                    >
+                        <line x1="5" y1="12" x2="19" y2="12" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default MapChart;
