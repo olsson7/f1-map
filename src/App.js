864913ce58c5a629f6f9cd27d5fa012be7b92028
olsson7 <72479962+olsson7@users.noreply.github.com>
2023-05-14 19:29:47 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./styles.css";
 
 import MapChart from "./MapChart";
+import NavBar from "./NavBar.js"
 
 export default function App() {
   return (
     <div>
+      <NavBar />
                   <h1> F1 World map!</h1>
       <MapChart  />
 
